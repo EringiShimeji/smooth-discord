379681952a89a71b7760f2e1f10114ecb7e5b56b
massyu5296 <h5a2p9p6iruku@gmail.com>
2021-08-23 00:43:28 +0900

    change template directory path

--- src/template/parsers/templateDirectoryParser.ts
+++ src/template/parsers/templateDirectoryParser.ts
@@ -3,6 +3,7 @@ import directoryParser from '../../utils/directoryParser';
 
 const templateDirectoryParser = async (
   type: 'javascript' | 'typescript',
-): Promise<string[]> => await directoryParser(`${root}/template/${type}`);
+): Promise<string[]> =>
+  await directoryParser(`${root}/node_modules/smooth-discord/template/${type}`);
 
 export default templateDirectoryParser;
